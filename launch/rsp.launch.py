478e4c4c426c68b66b664717ecb75b1255946d78
jainvrushab5 <jainvrushab5@gmail.com>
2024-04-12 22:31:38 +0530

    Fix_controller files

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -3,19 +3,23 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
 import xacro
 
 def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_ros2_control = LaunchConfiguration('use_ros2_control')
+
+
     pkg_path = get_package_share_directory('autonomous_bot')
     xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
     robot_description_config = xacro.process(xacro_file)
+    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
     
-    # Hardcoded path to the RViz config file
-    rviz_config_path = '/home/rushabh/ros2_ws/src/autonomous_bot/rviz/rviz.rviz'
+ 
 
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config}
@@ -44,5 +48,14 @@ def generate_launch_description():
 
 
     return LaunchDescription([
+         DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use sim time if true'),
+        DeclareLaunchArgument(
+            'use_ros2_control',
+            default_value='true',
+            description='Use ros2_control if true'),
+            
         node_robot_state_publisher,
     ])
